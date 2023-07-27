ec1c84be9d714a86bdaa548b3fe0190cecdeab60
akashydv311 <akashydv311@gmail.com>
2023-07-27 15:33:10 +0530

    week-7 assigenments

--- week-7/workplace-week-7/week-7-typescript/enum-typescript.ts
+++ week-7/workplace-week-7/week-7-typescript/enum-typescript.ts
@@ -1,16 +1,20 @@
 
-enum Arithemic {
-    Add,
-    Sub,
-    Div,
-    Mul
-}
+// enum Arithemic {
+//     Add,
+//     Sub,
+//     Div,
+//     Mul
+// }
 
-function calculate(a: number, b: number, type: Arithemic): number {
-    console.log(type);
+// function calculate(a: number, b: number, type: Arithemic): number {
+//     console.log(type);
 
-    return 1;
-}
+//     return 1;
+// }
 
-const ans = calculate(1, 2, Arithemic.Div);
-console.log(ans);
+// const ans = calculate(1, 2, Arithemic.Div);
+// console.log(ans);
+
+
+var x: number = 10;
+var y: string = "Akash";
